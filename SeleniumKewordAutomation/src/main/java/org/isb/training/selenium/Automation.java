2ee41a9251698355d0faf2ef7a636f6bb71e7839
Kabir Humayun <kabir0541@gmail.com>
2019-07-09 22:08:36 -0400

    07/09/2019 have done some changed.

--- SeleniumKewordAutomation/src/main/java/org/isb/training/selenium/Automation.java
+++ SeleniumKewordAutomation/src/main/java/org/isb/training/selenium/Automation.java
@@ -1,54 +1,40 @@
 package org.isb.training.selenium;
 
-import java.util.concurrent.TimeUnit;
-//--
-import org.openqa.selenium.By;
-//--
-import org.openqa.selenium.WebDriver;
-//--
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-//--
-//import org.openqa.selenium.firefox.FirefoxDriver;
-//--
-import org.testng.Assert;
-//--
-import org.testng.annotations.AfterClass;
-//--
-import org.testng.annotations.BeforeClass;
-//--
-import org.testng.annotations.Test;
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+//import org.zia.training.selenium.keyword.Automation;
+//import org.zia.training.selenium.keyword.Driver;
+//import org.zia.training.selenium.keyword.ExcelSheetDriver;
+
+import jxl.Sheet;
+import jxl.read.biff.BiffException;
 
 public class Automation {
 
-    private WebDriver driver;
+	final static Logger logger = Logger.getLogger(Automation.class);
+	public static void main(String[] args) throws BiffException, IOException {
+		
+//		Driver d = new Driver();
+		ExcelSheetDriver excelSheetDriver = new ExcelSheetDriver();
+		
+		Sheet testSuitesheet = excelSheetDriver.getWorksheet("./TestCases/TestSuite.xls", "Sheet1");
+		int c = excelSheetDriver.columnCount();
+		int r = excelSheetDriver.rowCount();
+		
+		for(int i = 1;i<r;i++)
+		{
+//			String SNo = excelSheetDriver.readCell(testSuitesheet,0, i);
+			String SNo = excelSheetDriver.readCell(testSuitesheet, 0, i);
 
-    @BeforeClass
-    public void beforeClass() {
-    	//copy from Test Automation src>driver.java> paste here chrome driver executable file
-		System.setProperty("webdriver.chrome.driver","./webdriver/chromedriver.exe");
-		// driver=new FirefoxDriver();
-//		driver = new ChromeDriver();
-        driver = new ChromeDriver();
-    }
-
-    @AfterClass
-    public void afterClass() {
-        driver.quit();
-    }
-
-    @Test
-    public void verifySearchButton() {
-
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-        driver.get("http://www.google.com");
-
-        String search_text = "Google Search";
-        WebElement search_button = driver.findElement(By.name("btnK"));
-
-        String text = search_button.getAttribute("value");
-
-        Assert.assertEquals(text, search_text, "Text not found!");
-    }
+			String Description = excelSheetDriver.readCell(testSuitesheet,1, i);
+			String ExecutionFlag = excelSheetDriver.readCell(testSuitesheet,2, i);
+//			logger.info("TestSuite:" + SNo);
+//			logger.info("TestSuite:" + Description);
+//			logger.info("TestSuite:" + ExecutionFlag);
+			System.out.println("TestSuite:" + SNo);
+			System.out.println("TestSuite:" + Description);
+			System.out.println("TestSuite:" + ExecutionFlag);
+		}
+	}
 }
